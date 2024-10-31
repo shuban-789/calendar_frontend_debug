ac1fb70819b513b7ecfc172c5f19262753492ca3
shuban-789 <cybergeek11929@gmail.com>
2024-10-31 12:43:13 -0700

    initialization

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "net/http"
+    "log"
+    "time"
+)
+
+func loggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+
+        rw := &responseWriter{w, http.StatusOK}
+
+        log.Printf("Received request: %s %s from %s 🟢", r.Method, r.URL.Path, r.RemoteAddr)
+        log.Printf("Request Headers: %+v 🟢", r.Header)
+
+        next.ServeHTTP(rw, r)
+
+        log.Printf("Responded with status: %d in %v 🟢", rw.statusCode, time.Since(start))
+    })
+}
+
+type responseWriter struct {
+    http.ResponseWriter
+    statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+    rw.statusCode = code
+    rw.ResponseWriter.WriteHeader(code)
+}
+
+func main() {
+    fs := http.FileServer(http.Dir("."))
+    http.Handle("/", loggingMiddleware(fs))
+
+    log.Println("Serving on http://localhost:3000 🟢")
+    err := http.ListenAndServe(":3000", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
